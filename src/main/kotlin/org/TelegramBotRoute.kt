18893f923472d64d68e276888b02d3b67f0a115c
ikurenkov <ikurenkov88@gmail.com>
2023-04-02 22:05:04 +0400

    initial

---
+++ src/main/kotlin/org/TelegramBotRoute.kt
@@ -0,0 +1,17 @@
+package org
+import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.component.telegram.TelegramConstants
+import org.apache.camel.component.telegram.model.IncomingMessage
+import javax.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class TelegramBotRoute : RouteBuilder() {
+
+    override fun cosnfigure() {
+        from("telegram:bots/{{botToken}}")
+            .filter { exchange -> exchange.getIn().getBody(IncomingMessage::class.java)?.text?.startsWith("/hello") == true }
+            .setHeader(TelegramConstants.TELEGRAM_CHAT_ID, simple("\${body.message.chat.id}"))
+            .setBody(simple("Hello, \${body.message.chat.firstName}!"))
+            .to("telegram:bots/{{botToken}}")
+    }
+}
