79a5f3db545e7e826061ec34bbe1c73661624897
ikurenkov <ikurenkov88@gmail.com>
2023-04-02 22:51:50 +0400

    initial

--- src/main/kotlin/org/TelegramBotRoute.kt
+++ src/main/kotlin/org/TelegramBotRoute.kt
@@ -8,10 +8,10 @@ import javax.enterprise.context.ApplicationScoped
 class TelegramBotRoute : RouteBuilder() {
 
     override fun configure() {
-        from("telegram:bots/{{botToken}}")
+        from("telegram:bots/123s")
             .filter { exchange -> exchange.getIn().getBody(IncomingMessage::class.java)?.text?.startsWith("/hello") == true }
             .setHeader(TelegramConstants.TELEGRAM_CHAT_ID, simple("\${body.message.chat.id}"))
             .setBody(simple("Hello, \${body.message.chat.firstName}!"))
-            .to("telegram:bots/{{botToken}}")
+            .to("telegram:bots/123")
     }
 }
